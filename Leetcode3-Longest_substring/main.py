a7c1f59ec4272024e0f78b6c29f467cc5db6a4d6
Animeshrockn <animeshrockn@gmail.com>
2022-12-10 02:11:01 -0800

    LC3

---
+++ Leetcode3-Longest_substring/main.py
@@ -0,0 +1,37 @@
+# Given a string s, find the length of the longest 
+# substring
+#  without repeating characters.
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# =============================================================
+
+s = "abcabcbb"
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ch = set()
+        l = r = 0
+        res = 0
+        
+        while r<len(s):
+            
+            if s[r] not in ch:
+                ch.add(s[r])
+                r +=1
+                res = max(res, r-l)
+            else:
+                ch.remove(s[l])
+                l +=1
+        return res
+
+sol = Solution()
+
+k = sol.lengthOfLongestSubstring(s)
+
+
+print(f'Output : {k}')
+
+
